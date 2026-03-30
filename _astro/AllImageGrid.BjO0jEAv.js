51bb28860a285195c8c37f2b6d9487a643afb405
Hugo Leal <hugolardosa@gmail.com>
2026-03-30 14:14:13 +0100

    change the favicon and fotter header

--- _astro/AllImageGrid.BjO0jEAv.js
+++
@@ -1,1 +0,0 @@
-import{G as i}from"./AllImageGrid.CBHwZqIk.js";import"./Image.DrqnZzXh.js";import"./index.By3MAEPo.js";import"./index.D-7hWLUd.js";export{i as default};
---
+++ _astro/AllImageGrid._7iW7qLp.js
@@ -0,0 +1,1 @@
+import{G as i}from"./AllImageGrid.MIUtik2q.js";import"./Image.DrqnZzXh.js";import"./index.By3MAEPo.js";import"./index.D-7hWLUd.js";export{i as default};
